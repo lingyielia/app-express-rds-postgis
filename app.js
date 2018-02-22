d71931d8ea7c3d1b65529edbae56a985c16d1f1e
Lingyi Zhang <Lingyi@Lingyis-MBP.fios-router.home>
2018-02-21 22:08:22 -0500

    v2

--- app.js
+++ app.js
@@ -11,42 +11,39 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
-//demo request to api
-// app.get("/results", function(req, res) {
-//     var query = 'Point(' + req.query.lonSearch + ' ' + req.query.latSearch + ')';
-//     var url = 'http://www.omdbapi.com/?i=tt3896198&apikey=thewdb' + query;
-//     request(url, function(error, response, body){
-//         if(!error && response.statusCode == 200){
-//             var parsedData = JSON.parse(body);
-//             var long = req.params.lon;
-//             var lati = req.params.lat;
-//             //res.send(parsedData['Title']);
-//             res.render("geocrosswalk", {longVar: long, latiVar: lati, data: parsedData});
-//         }
-//     })
-// })
-
-
 // query from AWS RDS directly
 app.get("/results", function(req, res){
+
+    var queryplace = req.query.place;
+    console.log(queryplace);
     var querypoint = 'Point(' + req.query.lonSearch + ' ' + req.query.latSearch + ')';
-    // var querypoint = 'Point(-118.159953387143 33.8719625444075)';
     const promise = require('bluebird'); // or any other Promise/A+ compatible library;
     const initOptions = {
         promiseLib: promise // overriding the default (ES6 Promise);
     };
     const pgp = require('pg-promise')(initOptions);
-    const connection = "postgres://username:password@rdsEndPoint:5432/DatabaseName";
+    const connection = "postgres://username:password@db2.c8qdkldhfwra.us-east-1.rds.amazonaws.com:5432/db_argo";
     const db = pgp(connection);
     console.log("Start Query");
-    var querytext = "SELECT * FROM db_argo WHERE ST_Contains (db_argo.the_geom, ST_GeomFromText('" + querypoint + "', 4326))=TRUE;";
+    if (queryplace == "nyc") {
+        var querytext = "SELECT * FROM db_nyc WHERE ST_Contains (db_nyc.the_geom, ST_GeomFromText('" + querypoint + "', 4326))=TRUE;";
+    } else {
+        var querytext = "SELECT * FROM db_argo WHERE ST_Contains (db_argo.the_geom, ST_GeomFromText('" + querypoint + "', 4326))=TRUE;";
+    };
+
     console.log(querytext);
     db.any(querytext, [true])
         .then(data => {
             console.log('DATA:', data); // print data;
             var long = req.query.lonSearch;
             var lati = req.query.latSearch;
-            res.render("geocrosswalk", {longVar: long, latiVar: lati, data: data});
+
+            if (queryplace == "nyc") {
+                res.render("geocrosswalkNYC", {longVar: long, latiVar: lati, data: data});
+            } else {
+                res.render("geocrosswalkLA", {longVar: long, latiVar: lati, data: data});
+            }
+
         })
         .catch(error => {
             console.log('ERROR:', error); // print the error;
@@ -54,7 +51,10 @@ app.get("/results", function(req, res){
         .finally(db.$pool.end); // For immediate app exit, shutting down the connection pool
     });
 
-
+// get api page
+app.get("/api", function(req, res) {
+    res.render("apidoc");
+});
 
 //return a message when url is wrong
 //should put in the end, as order sensitive in routing
